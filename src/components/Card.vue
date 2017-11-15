4e2cc2b0fb3bae95dcd5201c01a750a22e9c438b
c.simon.sanchez <c.simon.sanchez@accenture.com>
2017-11-15 13:25:05 +0100

    initial

---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div >
+      
+        <SymbolPoint :symbol="symbols[0]" @selectedSymbolCard="selectSymbol"/>
+        <SymbolPoint :symbol="symbols[1]" @selectedSymbolCard="selectSymbol"/>
+        <SymbolPoint :symbol="symbols[2]" @selectedSymbolCard="selectSymbol"/>
+
+    
+        <SymbolPoint :symbol="symbols[3]" @selectedSymbolCard="selectSymbol"/>
+        <SymbolPoint :symbol="symbols[4]" @selectedSymbolCard="selectSymbol"/>
+        <SymbolPoint :symbol="symbols[5]" @selectedSymbolCard="selectSymbol"/>
+        <SymbolPoint :symbol="symbols[6]" @selectedSymbolCard="selectSymbol"/>
+        <SymbolPoint :symbol="symbols[7]" @selectedSymbolCard="selectSymbol"/>
+
+  </div>
+  
+  
+</template>
+
+<script>
+import Vue from 'vue'
+import SymbolPoint from './Symbol.vue'
+
+export default Vue.component('Card', {
+  name: 'Card',
+  props : ['card'],
+  data () {
+    return {
+     symbols: [0, 7, 14, 21, 28, 35, 42, 49],
+     
+    }
+  },
+   methods : {
+        selectSymbol : function(symbol){
+            console.log('selectedSymbolCard '+symbol);
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Symbol.vue
@@ -0,0 +1,34 @@
+<template>
+  <div >
+    <h1 v-on:click="handleClick"> {{symbol}}</h1>
+    
+  </div>
+  
+  
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('SymbolPoint', {
+  name: 'SymbolPoint',
+  props : ['symbol'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+   methods : {
+        handleClick : function(){
+            console.log('selecting')
+            this.$emit('selectedSymbolCard',this.symbol);
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+
+</style>
+
+
