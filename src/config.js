0f5fc3ce65b7536f96b135f4caedf571c927a49a
c.simon.sanchez <c.simon.sanchez@accenture.com>
2018-02-28 10:44:55 +0100

    Auth0

---
+++ src/config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: 'AIzaSyDWFgrrF8o65RPq2xiW9LElqOGG0e4h6hY',
+  authDomain: 'crazycards-12b3b.firebaseapp.com',
+  databaseURL: 'https://crazycards-12b3b.firebaseio.com',
+  projectId: 'crazycards-12b3b',
+  storageBucket: 'crazycards-12b3b.appspot.com',
+  messagingSenderId: '699532579771'
+}
+export default config
--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import router from './router'
+import App from './Application.vue'
 import store from './gameStore'
 import _ from  'lodash';
 
-import firebase from 'firebase';
 
 
 
-firebase.initializeApp({
-    apiKey: "AIzaSyDb1sCq2HSN63Kw2QDUWWBuHvV8-QUCfas",
-    authDomain: "coches-ad68e.firebaseapp.com",
-    databaseURL: "https://coches-ad68e.firebaseio.com",
-    projectId: "coches-ad68e",
-    storageBucket: "coches-ad68e.appspot.com",
-    messagingSenderId: "479074765777"
-  });
 
 
 new Vue({
     el: '#app',
     store,
+    router,
     render: h => h(App)
 })
